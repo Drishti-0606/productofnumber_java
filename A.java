04fe627f703db6716c67fc385cd8cb5951139b38
Drishti-0606 <gohildrishti544@gmail.com>
2025-07-02 09:43:36 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class A{
+public static void main(String args[]){
+int num1,num2,res;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 1 no.");
+num1=sc.nextInt();
+System.out.println("enter 2 no.");
+num2=sc.nextInt();
+res=num1*num2;
+System.out.println("product is =" +res);
+}
+}
